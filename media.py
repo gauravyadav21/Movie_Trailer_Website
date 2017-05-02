364e60a605ee998e3815a45bcf329d14ed7a15f5
Gaurav Yadav <Gaurav Yadav>
2017-05-02 10:25:15 +0530

    media.py

--- media.py
+++ media.py
@@ -1,7 +1,7 @@
 import webbrowser
 
 class Movie():
-	'''This class provides a way to store movie related information.'''
+    '''This class provides a way to store movie related information.'''
     """
          title: This contains title of movie.
          storyline: This contains description of movie.
@@ -9,10 +9,10 @@ class Movie():
          trailer_youtube_url: This contains youtube url for trailer of a movie.
     """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-	
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+    
     def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+        webbrowser.open(self.trailer_youtube_url)
